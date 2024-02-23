044c7665e653d54fda7592595c4cd07de50e700d
Musfirah Lodhi <Musfirahlodhi4@gmail.com>
2024-02-23 17:16:42 +0500

    Assignment14

---
+++ app.js
@@ -0,0 +1,143 @@
+// ------Chap 1------
+
+// Q1
+var message = "Hello Welcome";
+alert(message)
+
+// Q2
+var error = "Error! Please enter a valid password";
+alert(error)
+
+// Q3
+var linebr = "Welcome to JS land...\nHappy Coding!";
+alert(linebr)
+
+// Q4
+var msg1 = "Welcome to JS land...";
+alert(msg1)
+var msg2 = "Happy Coding!";
+alert(msg2)
+
+// Q5
+console.log("Hello i can run js through my browser")
+// Q6..done & q7 see index.html
+
+
+
+// ------------Chap2-----------
+
+// Q1
+var username;
+
+// Q2
+var myname ="Musfirah Muzzammil Lodhi";
+
+// Q3
+var message = "hello World"
+alert(message)
+
+// Q4
+var name = "John Doe"
+alert(name)
+var age = "15 years"
+alert(age)
+var course = "Certified Mobile Application Development"
+alert(course)
+
+// Q5
+var pizza= "PIZZA\nPizz\nPiz\nPi\nP"
+alert(pizza)
+
+// Q6
+var email= "Musfirahlodhi4@gmail.com"
+alert("My email adress is "+email)
+
+// Q7
+var book ="“A smarter way to learn JavaScript”."
+alert("I am trying to learn js from"+book)
+
+// Q8
+document.write("Yah!I can write Html content through JS")
+
+// Q9
+var design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
+
+
+
+// --------Chap3---------
+
+// Q1
+var age = "I am 19 years old"
+alert(age)
+
+// Q2
+var n = "You have visited this site 10 times"
+alert(n)
+
+// Q3
+var birthyear ="2004"
+document.write("My birth year is " +birthyear)
+
+// Q5
+var visitorsname= "John doe"
+var producttitle= "T-shirts"
+var Quantity= "5"
+document.write(visitorsname+"ordered"+Quantity+producttitle+" form XYZ clothing store")
+
+// ----------Chap 1-3-----------
+// -----chapter1
+// Q1
+var firstname= "musfirah"
+var lastname="lodhi"
+var email="musfirahlodhi4@gmail.com"
+var phone= 0123456789
+var password="abc123"
+alert(firstname)
+alert(lastname)
+alert(email)
+alert(phone)
+alert(password)
+
+// Q2
+alert("You are learning JavaScript!")
+
+Q3
+alert("Never give up")
+
+// --------chapter2
+
+// Q1
+var FirstName
+
+// Q2
+var FirstName
+FirstName="Musfirahlodi"
+
+// Q3
+var TeamName = "Karachi Kings"
+alert(TeamName)
+
+// q4
+ var bestMan = "Musfirah"
+
+-----Chapter3
+
+// Q1
+var caseQty
+// Q2
+caseQty=144
+// Q3
+var num = +"9";
+// q4
+var num;
+num = 5 + 7;
+// q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+// q6
+var number = 30
+var newnumber = 10+number
